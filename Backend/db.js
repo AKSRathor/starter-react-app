f63d4bbf6739c7dc94ba9bae66011c94baa597be
AKSRathor <98573681+AKSRathor@users.noreply.github.com>
2023-02-21 22:38:17 +0530

    Add files via upload

---
+++ Backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const password = "BQrStvgn3gRZJwzf"
+const mongoURI = "mongodb://127.0.0.1:27017/portfolio"
+const uri = `mongodb+srv://aksr2003:${password}@portfolio.qrnpgxw.mongodb.net/?retryWrites=true&w=majority`
+
+const connectToMongoose =()=>{
+    // mongoose.set('strictQuery', true);
+    mongoose.connect(uri, ()=>{
+        console.log("connected to mongoose")
+    })
+    // .catch(err => console.log(err))
+
+}
+
+
+module.exports = connectToMongoose
