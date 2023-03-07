5a37b176ff14b450c7b0df5b42bdd6e22af0c7b1
AKSRathor <98573681+AKSRathor@users.noreply.github.com>
2023-03-07 13:08:51 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mongoose = require('mongoose')
 const password = "BQrStvgn3gRZJwzf"
 const mongoURI = "mongodb://127.0.0.1:27017/portfolio"
-const uri = process.env.MONGOLAB_URI || `mongodb+srv://aksr2003:${password}@portfolio.qrnpgxw.mongodb.net/?retryWrites=true&w=majority` 
+const uri = process.env.MONGOLAB_URI 
 
 // const uri = process.env.URI + "?retryWrites=true&w=majority"
 // console.log("THe code is ",urc)
